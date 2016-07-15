41151f9025489d97d167471c34ef7f77704b0325
Gralex <aleksandr.grigorchuk@gmail.com>
2016-07-15 14:16:58 +0300

    Complete development module Mariupolgaz.EContract.Finder.

--- Mariupolgaz.EContract.DocsContract/ViewModel/DocsContarctViewModel.cs
+++ Mariupolgaz.EContract.DocsContract/ViewModel/DocsContarctViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 using Mariupolgaz.EArchiv.Common.Models;
 
 namespace Mariupolgaz.EContract.DocsContract.ViewModel
@@ -11,5 +13,129 @@ namespace Mariupolgaz.EContract.DocsContract.ViewModel
 	/// </summary>
 	public class DocsContarctViewModel : BaseViewModel
 	{
+		#region Properties
+
+		/// <summary>
+		/// Список отсканированных документов для договора
+		/// </summary>
+		public ObservableCollection<Document> Documents { get; private set; }
+
+		private Document _seldoc;
+		/// <summary>
+		/// Текущий документа
+		/// </summary>
+		public Document SelectedDocument 
+		{ 
+			get { return _seldoc; }
+			set {
+				if(_seldoc != value) {
+					_seldoc = value;
+					RaisePropertyChanged(() => SelectedDocument);
+				}
+			}
+		}
+
+		private Contract _curcontro;
+		/// <summary>
+		/// Текущий договор
+		/// </summary>
+		public Contract CurrentContract
+		{
+			get { return _curcontro; }
+			set {
+				if(_curcontro != value) {
+					_curcontro = value;
+					RaisePropertyChanged(() => CurrentContract);
+				}
+			}
+		}
+
+		private bool _isDirty;
+		/// <summary>
+		/// Признак вносились ли изменения в коллекцию документов
+		/// </summary>
+		public bool IsDirty
+		{
+			get { return _isDirty; } 
+			set {
+				if(_isDirty != value) {
+					_isDirty = value;
+					RaisePropertyChanged(() => IsDirty);
+				}
+			}
+		}
+
+		#endregion
+
+		#region Commands
+
+		/// <summary>
+		/// Добавляем новый документ к договору
+		/// </summary>
+		public ICommand AddDocument
+		{
+			get { return new DelegateCommand(onAddDocument); }
+		}
+
+		private void onAddDocument()
+		{
+
+		}
+
+		/// <summary>
+		/// Редектирование выбранного документа
+		/// </summary>
+		public ICommand EditDocument
+		{
+			get { return new DelegateCommand(onEditDocument, canEditDocument); }
+		}
+
+		private void onEditDocument()
+		{
+
+		}
+
+		private bool canEditDocument()
+		{
+			return this.SelectedDocument != null;
+		}
+
+		/// <summary>
+		/// Удаление текущего документа
+		/// </summary>
+		public ICommand DeleteDocument
+		{
+			get { return new DelegateCommand(onDeleteDocument, canDeleteDocument); }
+		}
+
+		private void onDeleteDocument()
+		{
+
+		}
+
+		private bool canDeleteDocument()
+		{
+			return this.SelectedDocument != null;
+		}
+
+		/// <summary>
+		/// Сохраняет внесенные изменения
+		/// </summary>
+		public ICommand SaveChange
+		{
+			get { return new DelegateCommand(onSaveChange, canSaveChange); }
+		}
+
+		private void onSaveChange()
+		{
+
+		}
+
+		private bool canSaveChange()
+		{
+			return this.IsDirty;
+		}
+
+		#endregion
 	}
 }
--- Mariupolgaz.EContract.Finder/Services/ContractFinderService.cs
+++ Mariupolgaz.EContract.Finder/Services/ContractFinderService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Mariupolgaz.EArchiv.Common.Models;
 using Mariupolgaz.EArchiv.Common.Servises;
+using VanessaSharp.Data;
 
 namespace Mariupolgaz.EContract.Finder.Services
 {
@@ -20,29 +21,104 @@ namespace Mariupolgaz.EContract.Finder.Services
 		/// <returns>Список найденных контрагентов</returns>
 		public IList<Contragent> FindContragents(string orgKey, string contragentName)
 		{
-			IList<Contragent> rslt = null;
+			IList<Contragent> rslt = new List<Contragent>();
 
-			//TODO: Только в целях тестирования
-			rslt = new List<Contragent>();
-			IList<Contract> contracts = new List<Contract>();
+			var conBuilder = new OneSConnectionStringBuilder
+			{
+				ServerName = "serv",
+				ServerDatabaseName = "base_upp",
+				User = "Григорчук А М",
+				Password = "ufhujpfdh"
+			};
 
-			contracts.Add(new Contract("0001", "Contract 422", "422"));
-			contracts.Add(new Contract("0002", "Contract 42", "42"));
-			contracts.Add(new Contract("0003", "Contract 40", "40/117"));
-			rslt.Add(new Contragent("0001", "Рога и копыта", contracts));
-			contracts.Clear();
 
-			contracts.Add(new Contract("0004", "Contract 422", "422"));
-			contracts.Add(new Contract("0005", "Contract 42", "42"));
-			contracts.Add(new Contract("0006", "Contract 40", "40/117"));
-			rslt.Add(new Contragent("0002", "Рога и копыта", contracts));
-			contracts.Clear();
+			using (var con = new OneSConnection(conBuilder.ConnectionString)) {
+				string qrContragent = "ВЫБРАТЬ " +
+														"Контрагенты.Код КАК Код," +
+														"Контрагенты.Наименование," +
+														"Контрагенты.НаименованиеПолное," +
+														"Контрагенты.ИНН," +
+														"Контрагенты.КодПоЕДРПОУ," +
+														"Контрагенты.НомерСвидетельства " +
+											   "ИЗ Справочник.Контрагенты КАК Контрагенты " +
+											   "ГДЕ Контрагенты.Наименование ПОДОБНО &Наименование " +
+											   "УПОРЯДОЧИТЬ ПО Код";
+				string qrConcract = "ВЫБРАТЬ " +
+															"ДоговорыКонтрагентов.Код," +
+															"ДоговорыКонтрагентов.Наименование," +
+															"ДоговорыКонтрагентов.Номер," +
+															"ДоговорыКонтрагентов.Дата КАК Дата," +
+															"ДоговорыКонтрагентов.СрокДействия," +
+															"ДоговорыКонтрагентов.Подписан," +
+															"ДоговорыКонтрагентов.ЗакрытДосрочно," +
+															"ДоговорыКонтрагентов.ЗаявлениеПрисоединения," +
+															"ДоговорыКонтрагентов.ДатаЗаявленияПрисоединения," +
+															"ДоговорыКонтрагентов.ДатаЗакрытДосрочно " +
+														"ИЗ Справочник.ДоговорыКонтрагентов КАК ДоговорыКонтрагентов " +
+														"ГДЕ ДоговорыКонтрагентов.Владелец.Код = &Код " +
+														"УПОРЯДОЧИТЬ ПО Дата УБЫВ";
 
-			contracts.Add(new Contract("0007", "Contract 422", "422"));
-			contracts.Add(new Contract("0008", "Contract 42", "42"));
-			contracts.Add(new Contract("0009", "Contract 40", "40/117"));
-			rslt.Add(new Contragent("0003", "Рога и копыта", contracts));
-			contracts.Clear();
+				using (var cmd = new OneSCommand(con)) {
+					cmd.CommandText = qrContragent;
+					cmd.Parameters.Clear();
+					cmd.Parameters.AddNew(new OneSParameter("Наименование", "%" + contragentName + "%"));
+
+					con.Open();
+
+					using (var reader = cmd.ExecuteReader()) {
+
+						if (reader.HasRows) {
+							IList<Contract> contracts = new List<Contract>();
+							var cmdContract = new OneSCommand(con);
+							cmdContract.CommandText = qrConcract;
+
+							while (reader.Read()) {
+								string code = reader.GetString(0).Trim();
+								contracts.Clear();
+
+								cmdContract.Parameters.Clear();
+								cmdContract.Parameters.AddNew(new OneSParameter("Код", code));
+
+								var readerContracts = cmdContract.ExecuteReader();
+								if(readerContracts.HasRows) {
+									while(readerContracts.Read()) {
+										contracts.Add(new Contract(
+											readerContracts.GetString(0),
+											readerContracts.GetString(1),
+											readerContracts.GetString(2),
+											readerContracts.GetDateTime(3),
+											readerContracts.GetDateTime(4),
+											readerContracts.GetBoolean(5),
+											readerContracts.GetBoolean(6),
+											readerContracts.GetBoolean(7),
+											readerContracts.GetDateTime(8),
+											readerContracts.GetDateTime(9)
+										));
+									}
+								}
+								readerContracts.Close();
+								
+
+                rslt.Add(new Contragent(
+									code,
+									reader.GetString(1).Trim(),
+									contracts,
+									reader.GetString(2).Trim(),
+									reader.GetString(4),
+									reader.GetString(3),
+									reader.GetString(5)
+                ));
+								
+							}
+						}
+
+						reader.Close();
+					}
+				}
+			}
+
+			
+			
 
 			return rslt;
 		}
